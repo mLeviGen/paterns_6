7dd20d3bfec1516a41b68963df8458d891daa858
mLeviGen <milkovalexandr2004@gmail.com>
2026-05-26 23:43:26 +0200

    fix: rename deleteByUserId method to delete for consistency

--- src/services/AdminsService.js
+++ src/services/AdminsService.js
@@ -4,5 +4,5 @@ export class AdminsService {
     constructor(adminRepository) { this.adminRepository = adminRepository; }
     async createAdmin(admin) { return await this.adminRepository.insert(admin); }
     async getAllAdmins() { return await this.adminRepository.findAll(); }
-    async deleteAdmin(userId) { return await this.adminRepository.deleteByUserId(userId); }
+    async deleteAdmin(userId) { return await this.adminRepository.delete(userId); }
 }
